9a24f99b153c6b03b9940a4d3d33fac74b0ff856
Aleksandar Grbic <a@upbis.com>
2015-12-03 14:06:45 +0100

    path fixes

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
 (function(){
 "use strict";
 
-angular.module('CIRONS-MAIN-APP', ['ui.router', 'ngRoute', 'ngCookies', 'satellizer']);
+angular.module('CIRONS-MAIN-APP', [
+    'ui.router',
+    'ngRoute',
+    'ngCookies',
+    'satellizer',
+    'ncy-angular-breadcrumb'
+
+]);
 
 })();
 
