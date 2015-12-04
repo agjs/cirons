84b36292c248d22b37b8ad6b38dab8db652095d4
Aleksandar Grbic <a@upbis.com>
2015-12-04 02:43:30 +0100

    todo directive

--- app/components/directives/cirons-card/cirons_card.ngdirective.js
+++ app/components/directives/cirons-card/cirons_card.ngdirective.js
@@ -5,13 +5,6 @@ angular.module('CIRONS-MAIN-APP').directive('cironsCard', function(){
 
     return {
         templateUrl: 'components/directives/cirons-card/template.html',
-        restrict: 'E',
-        scope: {
-            color: '=color',
-            description: '=description',
-            icon: '=icon',
-            length: '=length'
-        }
     }
 
 });
