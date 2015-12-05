7251482005fa813ac989e6cc6e6cc7f4d33fde72
Aleksandar Grbic <a@upbis.com>
2015-12-05 18:06:01 +0100

    setting up atom

--- app/app.ngrun.js
+++ app/app.ngrun.js
@@ -1,20 +1,17 @@
-(function () {
-    "use strict";
+(function() {
+  "use strict";
 
-    angular.module('CIRONS-MAIN-APP').run(function ($rootScope, $state, meFactory, editableOptions, editableThemes) {
+  angular.module('CIRONS-MAIN-APP').run(function($rootScope, $state, meFactory, editableOptions, editableThemes) {
+
+    $rootScope.$state = $state;
+
+    // set `default` theme
+    editableOptions.theme = 'default';
+
+    // overwrite submit button template
+    editableThemes['default'].submitTpl = '<button type="submit">ok</button>';
 
 
-            $rootScope.$state = $state;
-
-            // set `default` theme
-            editableOptions.theme = 'default';
-
-            // overwrite submit button template
-            editableThemes['default'].submitTpl = '<button type="submit">ok</button>';
-
-
-        }
-    );
+  });
 
 })();
-
