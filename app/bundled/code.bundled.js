92a2cfff130a6d39e381fb2a4e45ddd48c4c0907
Aleksandar Grbic <a@upbis.com>
2015-12-21 18:50:48 +0100

    directive

--- app/bundled/code.bundled.js
+++ app/bundled/code.bundled.js
@@ -692,29 +692,33 @@ angular.module('CIRONS-MAIN-APP')
 
 },{}],29:[function(require,module,exports){
 (function() {
-    "use strict";
-    module.exports = cirons_list_card;
+  "use strict";
+  module.exports = cirons_list_card;
 
-    function cirons_list_card() {
+  function cirons_list_card() {
 
-      return {
-        restrict: 'EA',
-        scope: {
-            newitemstate: '@',
-            singleitemstate: '@',
-            data: '='
+    return {
+      restrict: 'EA',
+      scope: {
+        newitemstate: '@',
+        singleitemstate: '@',
+        data: '=',
+        p1: '@',
+        p2: '@',
+        p3: '@',
+        p4: '@'
 
-        },
-        templateUrl: 'components/directives/cirons-list-view/template.html',
-        replace: true,
-        link: function(scope) {
+      },
+      templateUrl: 'components/directives/cirons-list-view/template.html',
+      replace: false,
+      link: function(scope, element, attrs) {
 
-        }
       }
-
     }
 
-    cirons_list_card.$inject = [];
+  }
+
+  cirons_list_card.$inject = [];
 
 })();
 
