d64da03672e590ff43eaa2c443e51c0d9e40a9e5
Aleksandar Grbic <a@upbis.com>
2015-12-24 21:51:21 +0100

    dashboard

--- app/bundled/code.bundled.js
+++ app/bundled/code.bundled.js
@@ -597,12 +597,16 @@ angular.module('CIRONS-MAIN-APP')
   'use strict';
   module.exports = dashboardController;
 
-  function dashboardController($scope) {
-
-
+  function dashboardController($scope, $filter, dashboardFactory) {
+    dashboardFactory.getDashboardData().then(function(data) {
+      $scope.dashboardData = data;
+      $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total, '', 0);
+      $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, '', 0);
+      $scope.dashboardData.growth_mini = $filter('number')(data.growth_mini, 0) + '%';
+    })
   }
 
-  dashboardController.$inject = ['$scope'];
+  dashboardController.$inject = ['$scope', '$filter', 'dashboardFactory'];
 
 })();
 
@@ -704,20 +708,6 @@ angular.module('CIRONS-MAIN-APP')
   function dashboardFinanceController($scope, $filter, dashboardFactory) {
 
 
-
-    dashboardFactory.getDashboardData().then(function(data){
-      console.log(data);
-      $scope.dashboardData = data;
-      $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total,  '', 0);
-      $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, '', 0);
-
-
-
-
-
-
-
-    })
 
   }
 
--- app/components/dashboard/finance/dashboard_finance.ngcontroller.js
+++ app/components/dashboard/finance/dashboard_finance.ngcontroller.js
@@ -6,20 +6,6 @@
 
 
 
-    dashboardFactory.getDashboardData().then(function(data){
-      console.log(data);
-      $scope.dashboardData = data;
-      $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total,  '', 0);
-      $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, '', 0);
-
-
-
-
-
-
-
-    })
-
   }
 
   dashboardFinanceController.$inject = ['$scope', '$filter', 'dashboardFactory'];
