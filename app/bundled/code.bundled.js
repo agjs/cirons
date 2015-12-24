75db162fb73309173b3235b60a5ad6f4ac97627a
Aleksandar Grbic <a@upbis.com>
2015-12-24 21:43:38 +0100

    dashboard

--- app/bundled/code.bundled.js
+++ app/bundled/code.bundled.js
@@ -701,17 +701,27 @@ angular.module('CIRONS-MAIN-APP')
   'use strict';
   module.exports = dashboardFinanceController;
 
-  function dashboardFinanceController($scope, dashboardFactory) {
+  function dashboardFinanceController($scope, $filter, dashboardFactory) {
 
 
 
     dashboardFactory.getDashboardData().then(function(data){
+      console.log(data);
       $scope.dashboardData = data;
+      $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total,  data.due_invoices_mini.currency, 0);
+      $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, data.due_invoices_mini.currency, 0);
+
+
+
+
+
+
+
     })
 
   }
 
-  dashboardFinanceController.$inject = ['$scope', 'dashboardFactory'];
+  dashboardFinanceController.$inject = ['$scope', '$filter', 'dashboardFactory'];
 
 })();
 
