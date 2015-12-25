5390832e67477215b193ab2a356c4a54430c3e9f
Aleksandar Grbic <a@upbis.com>
2015-12-25 14:04:38 +0100

    updates

--- app/bundled/code.bundled.js
+++ app/bundled/code.bundled.js
@@ -599,7 +599,6 @@ angular.module('CIRONS-MAIN-APP')
 
   function dashboardController($scope, $filter, dashboardFactory) {
     dashboardFactory.getDashboardData().then(function(data) {
-      console.log(data);
       $scope.dashboardData = data;
       $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total, '', 0);
       $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, '', 0);
--- app/components/dashboard/dashboard.ngcontroller.js
+++ app/components/dashboard/dashboard.ngcontroller.js
@@ -8,7 +8,7 @@
       $scope.dashboardData.due_invoices_mini.total = $filter('currency')(data.due_invoices_mini.total, '', 0);
       $scope.dashboardData.turnover_this_month_mini.total = $filter('currency')(data.turnover_this_month_mini.total, '', 0);
       $scope.dashboardData.growth_mini = $filter('number')(data.growth_mini, 0) + '%';
-    })
+    });
   }
 
   dashboardController.$inject = ['$scope', '$filter', 'dashboardFactory'];
